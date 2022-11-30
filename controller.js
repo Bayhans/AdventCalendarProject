3ddc5fdb6f366891fe818789a96f8eed2074dea0
bayanKodehode <116548227+BayanKodehode@users.noreply.github.com>
2022-11-30 14:56:30 +0100

    Co-authored-by: Joakim Villo <VilloKodeHode@users.noreply.github.com>

---
+++ controller.js
@@ -0,0 +1,28 @@
+// const element = document.querySelector("content");
+// const compStyles = window.getComputedStyle(element);
+
+
+let content2 = document.querySelector("Luke2");
+const button2 = document.getElementById("btn2");
+
+
+function origin() {
+    window.moveTo(0, 0);
+  }
+
+// let clicks = 0
+
+// function changeImageSize() {
+//     content.style.width = "60%";
+//     content.style.height = "auto";
+//     content.style.transition = "width 0.5s ease";
+// }
+// // Function to reset image dimensions
+// function resetImg() {
+//     content.style.width = "40%";
+//     content.style.height = "auto";
+//     content.style.transition = "width 0.5s ease";
+// }
+
+
+//     if click += 1
--- view.js
+++ view.js
@@ -1,47 +1,95 @@
 show();
 function show() {
-    let mainPage = document.getElementById("app");
-    let tempPage = '';
-    tempPage += /*html*/`
-    <header>
-        <h1>Christmas Calendar 2022</h1>
-    </header>
+  let mainPage = document.getElementById("app");
+  let tempPage = "";
+  tempPage += /*html*/ `
+<header>
+  <h1>Christmas Calendar 2022</h1>
+</header>
 
-    <main>
-    <div class="luke1"><img src="./images/NewImage.jpg" width="80vw" heigh="50vw">
-    </div>
-    <div class="luke2"><p>2</p></div>
-     <div class="luke3"><p>3</p></div>
-    <div class="luke4"><p>4</p></div>
-     <div class="luke5"><p>5</p></div>
-    <div class="luke6"><p>6</p></div>
-    <div class="luke7"><p>7</p></div>
-    <div class="luke8"><p>8</p></div>
-    <div class="luke9"><p>9</p></div>
-    <div class="luke10"><p>10</p></div>
-    <div class="luke11"><p>11</p></div> 
-    <div class="luke12"><p>12</p></div>
-    <div class="luke13"><p>13</p></div>
-    <div class="luke14"><p>14</p></div>
-    <div class="luke15"><p>15</p></div>
-    <div class="luke16"><p>16</p></div>
-    <div class="luke17"><p>17</p></div>
-    <div class="luke18"><p>18</p></div>
-    <div class="luke19"><p>19</p></div>
-    <div class="luke20"><p>20</p></div>
-    <div class="luke21"><p>21</p></div>
-    <div class="luke22"><p>22</p></div>
-    <div class="luke23"><p>23</p></div> 
-    <div class="luke24"><p>24</p></div>
-                    <!-- <div class="empty"></div> -->
-    <div class="jul"> <p class="julP">Jul er Her</p></div>
-    </div>
+<main>
+  <div class="luke1 content" onClick="origin()">
+    <p>1</p><i class="fa-solid fa-candy-cane" style="color: red"></i>
+  </div>
+  <div class="luke2 content">
+      <p>2</p><i class="fa-solid fa-asterisk" style="color: red"></i>
+  </div>
+  <div class="luke3 content">
+      <p>3</p><i class="fa-solid fa-snowman" style="color: red"></i>
+  </div>
+  <div class="luke4 content">
+      <p>4</p><i class="fa-solid fa-sleigh" style="color: red"></i>
+  </div>
+  <div class="luke5 content">
+      <p>5</p><i class="fa-solid fa-snowflake" style="color: red"></i>
+  </div>
+  <div class="luke6 content">
+      <p>6</p><i class="fa-solid fa-stroopwafel" style="color: red"></i>
+  </div>
+  <div class="luke7 content">
+      <p>7</p><i class="fa-solid fa-gifts" style="color: red"></i>
+  </div>
+  <div class="luke8 content">
+      <p>8</p><i class="fa-solid fa-gift" style="color: red"></i>
+  </div>
+  <div class="luke9 content">
+      <p>9</p><i class="fa-solid fa-heart" style="color:red"></i>
+  </div>
+  <div class="luke10 content">
+      <p>10</p><i class="fa-sharp fa-solid fa-holly-berry" style="color: red"></i>
+  </div>
+  <div class="luke11 content">
+      <p>11</p><i class="fa-solid fa-candy-cane" style="color: red"></i>
+  </div>
+  <div class="luke12 content">
+      <p>12</p><i class="fa-solid fa-candy-cane" style="color: red"></i>
+  </div>
+  <div class="luke13 content">
+      <p>13</p><i class="fa-solid fa-asterisk" style="color: red"></i>
+  </div>
+  <div class="luke14 content">
+      <p>14</p><i class="fa-solid fa-candy-cane" style="color: red"></i>
+  </div>
+  <div class="luke15 content">
+      <p>15</p><i class="fa-solid fa-asterisk" style="color: red"></i>
+  </div>
+  <div class="luke16 content">
+      <p>16</p><i class="fa-solid fa-asterisk" style="color: red"></i>
+  </div>
+  <div class="luke17 content">
+      <p>17</p><i class="fa-solid fa-asterisk" style="color: red"></i>
+  </div>
+  <div class="luke18 content">
+      <p>18</p><i class="fa-solid fa-asterisk" style="color: red"></i>
+  </div>
+  <div class="luke19 content">
+      <p>19</p><i class="fa-solid fa-asterisk" style="color: red"></i>
+  </div>
+  <div class="luke20 content">
+      <p>20</p><i class="fa-solid fa-asterisk" style="color: red"></i>
+  </div>
+  <div class="luke21 content">
+      <p>21</p><i class="fa-solid fa-asterisk" style="color: red"></i>
+  </div>
+  <div class="luke22 content">
+      <p>22</p><i class="fa-solid fa-asterisk" style="color: red"></i>
+  </div>
+  <div class="luke23 content">
+      <p>23</p><i class="fa-solid fa-asterisk" style="color: red"></i>
+  </div>
+  <div class="luke24 content">
+      <p>24</p><i class="fa-solid fa-asterisk" style="color: green"></i>
+  </div>
+  <!-- <div class="empty"></div> -->
+  <div class="jul">
+      <p class="julP">Jul er Her</p>
+  </div>
+</main>
 
-    </main>
-    <footer>
- <div ><i class="fa-sharp fa-solid fa-star-christmas"></i></div>
-
-    </footer>
-    `;
-    mainPage.innerHTML = tempPage;
+  `;
+  mainPage.innerHTML = tempPage;
 }
+{
+  /*  */
+}
+
